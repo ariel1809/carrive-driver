563df40ee666b5072e2d92266cd4c2a45a6ebf61
ariel <stevekamga18@gmail.com>
2024-09-25 14:47:09 +0100

    init

---
+++ src/main/java/com/manage/carrivedriver/CarriveDriverApplication.java
@@ -0,0 +1,13 @@
+package com.manage.carrivedriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarriveDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarriveDriverApplication.class, args);
+    }
+
+}
