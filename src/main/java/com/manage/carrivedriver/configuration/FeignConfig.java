1274b9ea5cb27d66212d50e8611442350d9b82ea
ariel1809 <81252075+ariel1809@users.noreply.github.com>
2024-10-31 20:21:11 +0100

    send message

--- src/main/java/com/manage/carrivedriver/configuration/FeignConfig.java
+++ src/main/java/com/manage/carrivedriver/configuration/FeignConfig.java
@@ -2,7 +2,6 @@ package com.manage.carrivedriver.configuration;
 
 import com.manage.carrivedriver.security.JwtRequestFilter;
 import feign.RequestInterceptor;
-import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +10,9 @@ public class FeignConfig {
 
     @Bean
     public RequestInterceptor requestInterceptor() {
-        return new RequestInterceptor() {
-            @Override
-            public void apply(RequestTemplate template) {
-                String token = JwtRequestFilter.driver.getToken();
-                template.header("Authorization", "Bearer " + token);
-            }
+        return template -> {
+            String token = JwtRequestFilter.driver.getToken();
+            template.header("Authorization", "Bearer " + token);
         };
     }
 }
